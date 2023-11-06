a5b0ef3a0b3979fe857373523f77f6d9f896a0ea
pelageech <va.oleg2015@yandex.ru>
2023-11-06 13:52:48 +0700

    even more documentation

--- models/upgrade.go
+++ models/upgrade.go
@@ -1,13 +1,16 @@
 package models
 
-import "image"
-
+// Upgrade is an entity stores useful effects for towers.
 type Upgrade struct {
 	UpgradeConfig
-	Image     image.Image
+
+	// OpenLevel is a number of the level completing
+	// of which opens the upgrade.
 	OpenLevel int
 }
 
+// UpgradeConfig stores deltas for tower's parameters.
+// Applied upgrade adds to corresponding tower parameters these deltas.
 type UpgradeConfig struct {
 	DeltaDamage      int
 	DeltaSpeedAttack Frames
