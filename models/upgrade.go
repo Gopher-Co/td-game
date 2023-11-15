a21b52a3d2a9a7c2cfa8959dc98aec45f60b77fd
pelageech <va.oleg2015@yandex.ru>
2023-11-15 13:06:22 +0700

    upgrade typedef has been replaced with the who;e type struct

--- models/upgrade.go
+++ models/upgrade.go
@@ -1,4 +1,20 @@
 package models
 
 // Upgrade is an entity stores useful effects for towers.
-type Upgrade = UpgradeConfig
+type Upgrade struct {
+	Price            int
+	DeltaDamage      int
+	DeltaSpeedAttack Frames
+	DeltaRadius      Coord
+	OpenLevel        int
+}
+
+func NewUpgrade(config *UpgradeConfig) *Upgrade {
+	return &Upgrade{
+		Price:            config.Price,
+		DeltaDamage:      config.DeltaDamage,
+		DeltaSpeedAttack: config.DeltaSpeedAttack,
+		DeltaRadius:      config.DeltaRadius,
+		OpenLevel:        config.OpenLevel,
+	}
+}
