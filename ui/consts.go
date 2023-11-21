b1a08c868e15eba5cb66f470c5429cfdc4ca4de1
Dmitriy Krasnov <dekrasnov@avito.ru>
2023-11-21 22:02:35 +0700

    Add comments to the constants and functions in consts.go and ui.go for better code readability

--- ui/consts.go
+++ ui/consts.go
@@ -1,11 +1,24 @@
 package ui
 
 const (
-	MenuButtonPlayImage      = "MENU_BUTTON_PLAY_IMAGE"
-	MenuButtonReplaysImage   = "MENU_BUTTON_REPLAYS_IMAGE"
-	MenuButtonExitImage      = "MENU_BUTTON_EXIT_IMAGE"
-	MenuMainImage            = "MENU_MAIN_IMAGE"
-	MenuBackgroundImage      = "MENU_BACKGROUND_IMAGE"
-	MenuLeftSidebarImage     = "MENU_LEFT_SIDEBAR_IMAGE"
+	// MenuButtonPlayImage is a constant for the image of the play button.
+	MenuButtonPlayImage = "MENU_BUTTON_PLAY_IMAGE"
+
+	// MenuButtonReplaysImage is a constant for the image of the replays button.
+	MenuButtonReplaysImage = "MENU_BUTTON_REPLAYS_IMAGE"
+
+	// MenuButtonExitImage is a constant for the image of the exit button.
+	MenuButtonExitImage = "MENU_BUTTON_EXIT_IMAGE"
+
+	// MenuMainImage is a constant for the image of the main menu.
+	MenuMainImage = "MENU_MAIN_IMAGE"
+
+	// MenuBackgroundImage is a constant for the image of the menu background.
+	MenuBackgroundImage = "MENU_BACKGROUND_IMAGE"
+
+	// MenuLeftSidebarImage is a constant for the image of the menu left sidebar.
+	MenuLeftSidebarImage = "MENU_LEFT_SIDEBAR_IMAGE"
+
+	// LevelMenuBackButtonImage is a constant for the image of the level menu back button.
 	LevelMenuBackButtonImage = "LEVEL_MENU_BACK_BUTTON_IMAGE"
 )
