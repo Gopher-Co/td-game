798c8345ee06b50f7cb8e271b6e6117f121d524c
Dmitriy Krasnov <dekrasnov@avito.ru>
2023-11-21 21:21:03 +0700

    Add detailed comments to global variables in vars.go

--- global/vars.go
+++ global/vars.go
@@ -3,9 +3,18 @@ package global
 import "github.com/gopher-co/td-game/models"
 
 var (
-	UI      = make(models.UI)
-	Maps    = make(map[string]*models.MapConfig)
-	Levels  = make(map[string]*models.LevelConfig)
-	Towers  = make(map[string]*models.TowerConfig)
+	// UI is a map of images used in the game.
+	UI = make(models.UI)
+
+	// Maps is a map of maps used in the game.
+	Maps = make(map[string]*models.MapConfig)
+
+	// Levels is a map of levels used in the game.
+	Levels = make(map[string]*models.LevelConfig)
+
+	// Towers is a map of towers used in the game.
+	Towers = make(map[string]*models.TowerConfig)
+
+	// Enemies is a map of enemies used in the game.
 	Enemies = make(map[string]*models.EnemyConfig)
 )
