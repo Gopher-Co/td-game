21dfc3245c4d59663b76fb81d1752c0123994924
Dmitriy Krasnov <dekrasnov@avito.ru>
2023-11-21 21:52:39 +0700

    Add comments to the Projectile struct and its methods in projectile.go for better code readability

--- models/projectile.go
+++ models/projectile.go
@@ -8,18 +8,38 @@ import (
 // and deals the damage to it.
 // Projectiles never misses the enemy and achieves the aim when TTL is equal to zero.
 type Projectile struct {
-	Pos         Point // current position on a map
-	Vrms        Coord
-	Vx          Coord
-	Vy          Coord
-	Type        TypeAttack
-	Damage      int
-	TTL         Frames
+	// Pos is a position of the projectile.
+	Pos Point
+
+	// Vrms is a root mean square speed of the projectile.
+	Vrms Coord
+
+	// Vx is a speed of the projectile on the X axis.
+	Vx Coord
+
+	// Vy is a speed of the projectile on the Y axis.
+	Vy Coord
+
+	// Type is a type of the projectile.
+	Type TypeAttack
+
+	// Damage is a damage of the projectile.
+	Damage int
+
+	// TTL is a time to live of the projectile.
+	TTL Frames
+
+	// TargetEnemy is an enemy that the projectile is flying to.
 	TargetEnemy *Enemy
-	Image       *ebiten.Image
-	dead        bool
+
+	// Image is an image of the projectile.
+	Image *ebiten.Image
+
+	// dead is a flag that shows if the projectile is dead.
+	dead bool
 }
 
+// Update updates the projectile.
 func (p *Projectile) Update() {
 	p.move()
 	if !p.dead && p.TTL == 0 {
@@ -28,13 +48,14 @@ func (p *Projectile) Update() {
 	}
 }
 
+// Draw draws the projectile.
 func (p *Projectile) Draw(screen *ebiten.Image) {
 	geom := ebiten.GeoM{}
 	geom.Translate(float64(p.Pos.X-float32(p.Image.Bounds().Dx()/2)), float64(p.Pos.Y-float32(p.Image.Bounds().Dy()/2)))
 	screen.DrawImage(p.Image, &ebiten.DrawImageOptions{GeoM: geom})
 }
 
-// Move moves the projectile on the map.
+// move moves the projectile on the map.
 func (p *Projectile) move() {
 	p.Pos.X += p.Vx
 	p.Pos.Y += p.Vy
