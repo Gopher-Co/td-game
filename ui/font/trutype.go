f00d3192f5fa457b12dff202c9eedea74dd22edd
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-06-21 23:18:12 +0700

    font sizes

--- ui/font/trutype.go
+++ ui/font/trutype.go
@@ -3,13 +3,30 @@ package font
 import "github.com/gopher-co/td-game/ui/loaders"
 
 var (
-	TTF20  = loaders.FontTrueType(20)
-	TTF32  = loaders.FontTrueType(32)
-	TTF36  = loaders.FontTrueType(36)
-	TTF40  = loaders.FontTrueType(40)
-	TTF48  = loaders.FontTrueType(48)
-	TTF54  = loaders.FontTrueType(54)
-	TTF64  = loaders.FontTrueType(64)
-	TTF72  = loaders.FontTrueType(72)
+	// TTF20 is a font with size 20.
+	TTF20 = loaders.FontTrueType(20)
+
+	// TTF32 is a font with size 32.
+	TTF32 = loaders.FontTrueType(32)
+
+	// TTF36 is a font with size 36.
+	TTF36 = loaders.FontTrueType(36)
+
+	// TTF40 is a font with size 40.
+	TTF40 = loaders.FontTrueType(40)
+
+	// TTF48 is a font with size 48.
+	TTF48 = loaders.FontTrueType(48)
+
+	// TTF54 is a font with size 54.
+	TTF54 = loaders.FontTrueType(54)
+
+	// TTF64 is a font with size 64.
+	TTF64 = loaders.FontTrueType(64)
+
+	// TTF72 is a font with size 72.
+	TTF72 = loaders.FontTrueType(72)
+
+	// TTF128 is a font with size 128.
 	TTF128 = loaders.FontTrueType(128)
 )
