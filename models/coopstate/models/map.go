5c4c91da059261cdc63bc60da36e2f083537cce4
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-06-21 22:54:59 +0700

    Towers is a list of towers that can be built on the map.

--- models/coopstate/models/map.go
+++ models/coopstate/models/map.go
@@ -4,6 +4,7 @@ import "github.com/gopher-co/td-game/models/ingame"
 
 // Map represents a map.
 type Map struct {
+	// Towers is a list of towers that can be built on the map.
 	Towers []*Tower
 	Path   ingame.Path
 }
