3c33940b9d0b2af74443cc51c9ff0966717f674f
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-06-21 23:10:27 +0700

    handleUpgrade handles the upgrade button click

--- models/gamestate/button_handlers.go
+++ models/gamestate/button_handlers.go
@@ -53,6 +53,7 @@ func (s *GameState) handleTowerTake(v *config.Tower) func(eventArgs *widget.Butt
 	}
 }
 
+// handleUpgrade handles the upgrade button click.
 func (s *GameState) handleUpgrade(_ *widget.ButtonClickedEventArgs) {
 	s.upgradeTowerHandler(s.chosenTower)
 
